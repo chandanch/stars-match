247a703afd2e69db882f1cd80640f3cb92ff7461
chandanch <chandan.cec107@gmail.com>
2020-09-06 20:11:11 +0530

    move useState to separate file :recycle:

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,43 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+
 import StarsDisplay from './StarDisplay';
 import PlayNumber from './PlayNumber';
 import PlayAgain from './PlayAgain';
 import utils from '../math-utils';
-
-const useGameState = () => {
-  const [stars, setStars] = useState(utils.random(1, 9));
-  const [candidateNums, setCandidateNums] = useState([]);
-  const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
-  const [secondsLeft, setSecondsLeft] = useState(10);
-
-  useEffect(() => {
-    if (secondsLeft > 0 && availableNums.length > 0) {
-      const timerId = setTimeout(() => {
-        setSecondsLeft(secondsLeft - 1);
-      }, 1000);
-      return () => {
-        clearTimeout(timerId);
-      };
-    }
-  });
-
-  const setGameState = (newCandidateNums) => {
-    if (utils.sum(newCandidateNums) !== stars) {
-      setCandidateNums(newCandidateNums);
-    } else {
-      const newAvailableNums = availableNums.filter(
-        (number) => !newCandidateNums.includes(number),
-      );
-
-      // set star
-      setStars(utils.randomSumIn(newAvailableNums, 9));
-      setAvailableNums(newAvailableNums);
-      setCandidateNums([]);
-    }
-  };
-
-  return { stars, candidateNums, availableNums, secondsLeft, setGameState };
-};
+import useGameState from '../hooks/useGameState';
 
 // STAR MATCH - Starting Template
 const Game = (props) => {
